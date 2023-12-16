fb622071435a5484513bf56e454e2cbf2132270a
back <bgwbgw1001@gmial.com>
2023-12-16 15:03:43 +0900

    환경변수로 아이디 비번설정

--- src/main/java/bgw/board/config/SecurityConfig.java
+++ src/main/java/bgw/board/config/SecurityConfig.java
@@ -4,7 +4,14 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.jaas.memory.InMemoryConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,5 +24,36 @@ public class SecurityConfig {
                         .and().formLogin()
                         .and().httpBasic();
         return http.build();
+    }
+    @Bean
+    public InMemoryUserDetailsManager userDetailService(){
+       /* UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .authorities("read")
+                .build();
+        return  new InMemoryUserDetailsManager(admin,user);*/
+        UserDetails admin = User.withUsername("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+
+        UserDetails user = User.withUsername("user")
+                .password("12345")
+                .authorities("read")
+                .build();
+        return  new InMemoryUserDetailsManager(admin,user);
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
